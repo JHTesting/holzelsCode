41e5645d8eec14d05c6b6888ae8a440c7c07fc45
holzel <spenser.vale.com@gmail.com>
2017-11-15 11:28:38 -0600

    added fromHunto program

---
+++ Jfiles/fromHunto.java
@@ -0,0 +1,25 @@
+//This program will count down from 100 to 1(will print num);
+
+
+public class fromHunto {
+	public static void main(String[] args) {
+		int num = 100; //create var num and set to 100
+		while (num > 0) {
+			System.out.println(num); //print num
+			num = num - 1; //subtract 1 from num until 0 is reached then break
+		
+		
+		
+		
+		}
+	
+	
+	
+	
+	}
+
+
+
+
+
+}
