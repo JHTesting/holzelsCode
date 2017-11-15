26268c57fca50afe26347d1795cafba41aa59d58
holzel <spenser.vale.com@gmail.com>
2017-11-15 11:18:10 -0600

    added fromOneto program

---
+++ Jfiles/fromOneto.java
@@ -0,0 +1,25 @@
+//This program will increase num var by 1 until it reaches 100(will print too);
+
+public class fromOneto {
+	public static void main(String[] args) {
+		int num = 0;
+
+		while (num < 101) {
+			System.out.println(num);
+			num++;
+		
+		
+		
+		} 
+
+	
+	
+	
+	
+	}
+
+
+
+
+
+} 
