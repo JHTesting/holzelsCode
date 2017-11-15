360703ad990460c0aa8a9a8762316a10fff4b083
holzel <spenser.vale.com@gmail.com>
2017-11-15 11:39:41 -0600

    added fromtwoto

---
+++ Jfiles/fromTwoto.java
@@ -0,0 +1,21 @@
+//This program will print all even numbers between 0 and 100
+
+public class fromTwoto {
+	public static void main(String[] args) {
+		int num = 0; //create var num and assign 0 value
+		while (num < 101) {
+			System.out.println(num);
+			num = num + 2;
+		
+		
+		}
+	
+	
+	
+	
+	}
+
+
+
+
+}
