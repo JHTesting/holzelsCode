ea261333491a3da59d254c26ae3709e4bcfecaa2
holzel <spenser.vale.com@gmail.com>
2017-11-22 12:15:33 -0600

    added exercise 37

---
+++ Jfiles/methods.java
@@ -0,0 +1,19 @@
+//This program will print using a method
+
+import java.util.Scanner;
+public class methods {
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		printText(); //Call printText method
+		System.exit(0);
+
+
+	
+	}
+	public static void printText() {
+		System.out.println("In the beginning there were the swamp, the hoe and Java.");
+	
+	}
+
+
+}
